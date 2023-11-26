0137588af8249e5ecaae4cf40dca92e4ef134bc2
azara <yyushahu02@gmail.com>
2023-11-26 19:58:04 +0000

    pythoning

---
+++ mult_str.py
@@ -0,0 +1,6 @@
+# strings that pan multiple lines
+
+print('''\
+Usage: thingy [OPTIONS]
+     -h                      Display this usage message
+     -H hostname             Hostname to connect to''')
---
+++ width.py
@@ -0,0 +1,7 @@
+# using the equal sign (=) to assign a value to a variable
+
+width = 20
+height = 5 * 9
+vol = width * height
+
+print('vol = ', vol)
