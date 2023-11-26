8bc8216492b5425d8f69476ca9b243989ab1d5ee
azara <yyushahu02@gmail.com>
2023-11-26 20:46:11 +0000

    lists

---
+++ lists.py
@@ -0,0 +1,9 @@
+# this code will attempt to change an item in a list
+
+cubes = [1, 8, 27, 65, 125] # this is a list of the cube of 1, 2, 3, 4 and 5
+print('Given list =', cubes)
+print('Nay! One item is wrong\nLet\'s fix it!!\n')
+
+cubes[3] = 64
+print('Yay! Fixed')
+print('Updated list =', cubes)
