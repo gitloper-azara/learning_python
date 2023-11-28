f17608cb99d42629199e2ac5aa3f0ff87e2eef98
azara <yyushahu02@gmail.com>
2023-11-28 14:16:56 +0000

    average

---
+++ average.py
@@ -0,0 +1,9 @@
+# for loops
+
+n = eval(input('How many numbers do you have? '))
+total = 0.0
+
+for i in range(n):
+    x = float(input('Enter a number >> '))
+    total = total + x
+print(f'\nThe average of the numbers is: {total / n}')
