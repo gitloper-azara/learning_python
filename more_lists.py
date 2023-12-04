6d0d49ba7df1e2a626de63ebee121c21930bdf7e
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-04 15:55:23 +0000

    more lists

---
+++ more_lists.py
@@ -0,0 +1,14 @@
+'''
+we are going to do more lists for
+a data structure
+'''
+
+lst = ['one', 'two', 'three', 'four', 'five']
+print(lst)
+# trying the append way
+lst.append('six')
+print(lst)
+
+#trying the array way
+lst[len(lst):] = ['seven']
+print(lst)
