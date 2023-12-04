e10fa86b7056cc88b4465c37f3d9bfd3384d7cc4
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-04 16:15:38 +0000

    LIFO

---
+++ lists_as_stacks.py
@@ -0,0 +1,14 @@
+# trying LIFO with lists
+
+trythis = [10, 20, 60, 90, 40, 8]
+print('Printing the list first')
+print(trythis)
+print('Adding the integer')
+trythis.append(int(input('Insert an integer: ')))
+print(trythis)
+print('Deleting the last integer')
+trythis.pop()
+print('Integer deleted successfully')
+print(trythis)
+
+print('Last In First Out(LIFO) executed successfully...')
