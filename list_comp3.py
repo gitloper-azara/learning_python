ced7f0c59a14fc7603b55042045b36ebd07b002d
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-04 20:12:00 +0000

    additions

--- list_comp3.py
+++ list_comp3.py
@@ -16,9 +16,20 @@ for x in vec:
     srt2.append(abs(x))
 print(srt2)
 
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+transposed = []
+for i in range(4):
+    transposed.append([row[i] for row in matrix])
+print(transposed)
+
 print()
 # or
 vec1 = [-4, -2, 0, 2, 4]
 print([x * 2 for x in vec1])
 print([x for x in vec1 if x >= 0])
 print([abs(x) for x in vec1])
+print([[row[i] for row in matrix] for i in range(4)])
