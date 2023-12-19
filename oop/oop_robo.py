3afaab6aba61e6216a54b0dce70393afd02c6b1d
Yushahu Yussif <yyushahu02@gmail.com>
2023-12-19 17:36:36 +0000

    add setter and getter methods

--- oop/oop_robo.py
+++ oop/oop_robo.py
@@ -15,6 +15,17 @@ class Robot:
             print('Hi, I am {}!'.format(self.name))
         else:
             print('I am a robot without a name')
+    
+
+    def set_name(self, name):
+        """setter method for modifying the value of name"""
+        self.name = name
+
+
+    def get_name(self):
+        """getter method for retriving the value of the name
+        attribute"""
+        return self.name
 
 x = Robot(None,'1979', 'Kuka')
 y = Robot('Caliban', '1993', 'Tayata')
@@ -25,3 +36,7 @@ print(y.build_year)
 print(x.__dict__)
 print(y.__dict__)
 print(x.brand)
+x.set_name('Azara')
+x.hi()
+print(x.get_name())
+print(x.__dict__)
