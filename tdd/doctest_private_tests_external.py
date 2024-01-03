031b3c85ad155dbd19dcd85375c29a19802a9c68
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-03 23:21:54 +0000

    delete redundant files

--- tdd/doctest_private_tests_external.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""External tests associated with private_tests.py
-
->>> my_func(['A', 'B', 'C'], 2)
-['A', 'B', 'C', 'A', 'B', 'C']
-"""
--- tdd/in_help.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def my_func(a, b):
-    """Returns a * b.
-    """
-    return a * b
